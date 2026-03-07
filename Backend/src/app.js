fbe58c88827f27e20edeeaef86fad0d4190809b4
dhruvdiwakar01 <dhruvdiwakar.1@gmail.com>
2026-03-07 16:40:50 +0530

    ready for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
 }))
 app.use(express.static("./public"))
 
